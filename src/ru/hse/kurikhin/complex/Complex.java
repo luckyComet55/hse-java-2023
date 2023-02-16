3cb4583d8423c9822079912f33c64d87190658c7
Ivan Kurikhin <ivkurikhin@edu.hse.ru>
2023-02-16 22:55:19 +0300

    Added comments for Complex

--- src/ru/hse/kurikhin/complex/Complex.java
+++ src/ru/hse/kurikhin/complex/Complex.java
@@ -23,15 +23,31 @@ public class Complex {
   public double getB() { return b; }
   public void setB(double b) { this.b = b; }
 
+  /**
+   * Прибавление к себе переданного комплексного числа
+   * @param other комплексное число класса Complex
+   */
   public void addComplex(Complex other) {
     this.a += other.a;
     this.b += other.b;
   }
 
+  /**
+   * Сумма двух комплексных чисел класса Complex
+   * @param first комплексное число класса Complex
+   * @param second комплексное число класса Complex
+   * @return комплексное число результат сложения
+   */
   public static Complex sum(Complex first, Complex second) {
     return new Complex(first.a + second.a, first.b + second.b);
   }
 
+  /**
+   * Произведение двух комплексных чисел
+   * @param first комплексное число класса Complex
+   * @param second комплексное число класса Complex
+   * @return комплексное число результат произведения
+   */
   public static Complex product(Complex first, Complex second) {
     return new Complex(
         first.a * second.a - first.b * second.b,
@@ -39,11 +55,18 @@ public class Complex {
     );
   }
 
+  /**
+   * Умножение себя на переданное комплексное число класса Complex
+   * @param other комплексное число класса Complex
+   */
   public void multiplyComplex(Complex other) {
     this.a = this.a * other.a - this.b * other.b;
     this.b = this.a * other.b + this.b * other.a;
   }
 
+  /**
+   * Вывод комплексного числа в виде a + bi
+   */
   public void printRegular() {
     if (a == 0 && b == 0) {
       System.out.println("0");
@@ -53,10 +76,16 @@ public class Complex {
     System.out.println(out);
   }
 
+  /**
+   * Вывод комплексного числа в виде вектора
+   */
   public void printVector() {
     System.out.print("(" + a + "," + b + ")");
   }
 
+  /**
+   * Вывод комлексного числа в тригонометрической форме
+   */
   public void printTrigonometrical() {
     if (a == 0 && b == 0) {
       System.out.println("0");
@@ -69,6 +98,10 @@ public class Complex {
     System.out.println(out);
   }
 
+  /**
+   * Ввод комплексного числа
+   * @return комплексное число класса Complex
+   */
   public static Complex inputComplex() {
     System.out.println("Введите параметры комплексного числа:");
     Scanner in = new Scanner(System.in);
