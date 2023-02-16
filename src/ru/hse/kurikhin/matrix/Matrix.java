8f9d5eeb7a48fe34ac139057584b5b01a850f310
Ivan Kurikhin <ivkurikhin@edu.hse.ru>
2023-02-16 22:49:29 +0300

    Добавил комментарии для Matrix

--- src/ru/hse/kurikhin/matrix/Matrix.java
+++ src/ru/hse/kurikhin/matrix/Matrix.java
@@ -17,6 +17,10 @@ public class Matrix {
     matrix = new Complex[x][y];
   }
 
+  /**
+   * @return возвращает пустую матрицу с заданным размером
+   * @throws IllegalArgumentException выбрасывает ошибку, если размеры матрицы несоответствуют требованиям
+   */
   public static Matrix createEmptyMatrix() throws IllegalArgumentException {
     Scanner in = new Scanner(System.in);
     System.out.println("Введите размеры матрицы (размер не может быть меньше либо равен нулю):");
@@ -28,6 +32,9 @@ public class Matrix {
     return new Matrix(x, y);
   }
 
+  /**
+   * @param matrix получает на вход матрицу класса Matrix, которую надо заполнить элементами
+   */
   public static void inputMatrix(Matrix matrix) {
     Scanner in = new Scanner(System.in);
     System.out.println("Введите элементы матрицы:");
@@ -40,6 +47,9 @@ public class Matrix {
     }
   }
 
+  /**
+   * Выводит матрицу
+   */
   public void printMatrix() {
     for (int i = 0; i < x; ++i) {
       for (int j = 0; j < y; ++j) {
@@ -50,9 +60,25 @@ public class Matrix {
     }
   }
 
+  /**
+   * Геттер для X
+   * @return возвращает размерность матрицы по X
+   */
   public int getX() { return x; }
+
+  /**
+   * Геттер для Y
+   * @return возвращает размерность матрицы по Y
+   */
   public int getY() { return y; }
 
+  /**
+   * Сумма двух матриц одинаковых размеров
+   * @param first матрица класса Matrix
+   * @param second матрица класса Matrix
+   * @return матрица - результат суммы матриц
+   * @throws IllegalArgumentException выбрасывается, если переданные матрицы разных размеров
+   */
   public static Matrix sum(Matrix first, Matrix second) throws IllegalArgumentException {
     if (first.x != second.x || first.y != second.y) {
       throw new IllegalArgumentException("Переданы матрицы разных размеров");
@@ -66,6 +92,13 @@ public class Matrix {
     return sum;
   }
 
+  /**
+   * Перемножение матриц
+   * @param first матрица класса Matrix
+   * @param second матрица класса Matrix
+   * @return возвращает матрицу результат произведения, размер соответсвует математическим правилам
+   * @throws IllegalArgumentException выбрасывается, если перемножение невозможно
+   */
   public static Matrix product(Matrix first, Matrix second) throws IllegalArgumentException {
     if (first.y != second.x) {
       throw new IllegalArgumentException("Перемножение матриц с данными размерами невозможно");
@@ -83,6 +116,10 @@ public class Matrix {
     return product;
   }
 
+  /**
+   * Транспониурет матрицу, не изменяет исходную
+   * @return матрица результат транспонирования
+   */
   public Matrix transform() {
     Matrix transformed = new Matrix(y, x);
     for (int i = 0; i < y; ++i) {
